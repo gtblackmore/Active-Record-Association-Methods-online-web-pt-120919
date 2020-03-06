13be609caa224d0e45b9983aa19aa0a385bdc418
George Blackmore <gtblackmore@gmail.com>
2020-03-06 17:24:00 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -7,10 +7,10 @@ class Genre < ActiveRecord::Base
   end
 
   def artist_count
-    self.artist.length
+    self.artists.length
   end
 
   def all_artist_names
-    self.artist.all
+    self.artist.map{|a| :name}
   end
 end
